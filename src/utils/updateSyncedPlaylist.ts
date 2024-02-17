50ada96f49ca70eaf7c485d3ddd7316576eedf8c
Dimitri-chln <dimitrichaline4@gmail.com>
2024-02-17 20:39:34 +0100

    Catch errors

--- src/utils/updateSyncedPlaylist.ts
+++ src/utils/updateSyncedPlaylist.ts
@@ -3,12 +3,16 @@ import clearSyncedPlaylist from "./clearSyncedPlaylist.js";
 import fillSyncedPlaylist from "./fillSyncedPlaylist.js";
 
 export default async function updateSyncedPlaylist() {
-	console.log("Updating synced playlist...");
-	console.log(" - Loading saved tracks...");
-	const savedTracks = await loadSavedTracks();
-	console.log(" - Clearing the synced playlist...");
-	await clearSyncedPlaylist();
-	console.log(" - Adding the saved tracks back...");
-	await fillSyncedPlaylist(savedTracks.map((savedTrack) => savedTrack.track.uri));
-	console.log(" - Synced playlist successfully updated");
+	try {
+		console.log("Updating synced playlist...");
+		console.log(" - Loading saved tracks...");
+		const savedTracks = await loadSavedTracks();
+		console.log(" - Clearing the synced playlist...");
+		await clearSyncedPlaylist();
+		console.log(" - Adding the saved tracks back...");
+		await fillSyncedPlaylist(savedTracks.map((savedTrack) => savedTrack.track.uri));
+		console.log(" - Synced playlist successfully updated");
+	} catch (err) {
+		console.error(err);
+	}
 }
